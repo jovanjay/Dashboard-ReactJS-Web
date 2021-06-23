d9dd7190522ddbff4e80b8e4399fadc1e2ee4293
Jovan Jay Villamor <jovan.jay.v@gmail.com>
2021-06-23 17:22:33 +1000

    add deploy bash script fix issues with package.json

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Deploy - Verson 1.0
+
+#get params
+#Help : "-h"
+
+#Note : run the line of code to force to use node version and extend node option for max_old_space_size 
+# nvm use 12.8 && export NODE_OPTIONS=--max_old_space_size=1024 
+
+#help
+if [ "$1" == "-h" ]; then 
+
+  echo "help yourself..."
+
+elif [ "$1" == "-site" ] && [ "$2" == "reactjs" ]; then
+  if [ "$3" == "-clean" ]; then
+    echo "Get source from GIT"
+    git fetch --all --prune && git checkout . && git pull
+  fi
+
+  echo "Build file"
+  npm run build
+
+  echo "Navigate to public site"
+  cd ../reactjs.jovanjay.com && pwd
+
+  #delete all files
+  cdir=${PWD##*/}
+  if [ "$cdir" == "reactjs.jovanjay.com" ]; then
+    
+    #use -I for prompting user
+    rm -RI *
+
+    cp -R ../reactjs/build/* .
+    vim index.html
+  fi
+fi
